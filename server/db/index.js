5ff73d238c5222f13dd6a141d89f6dd91410f0c2
aaronub <wo-08@hotmail.com>
2022-08-19 11:46:45 -0400

    partial solution

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,21 @@
 const db = require('./db')
+const Album = require('./Album');
+const Artist = require('./Artist');
+const Song = require('./Song')
+
+
 // require each of your models here...
 
 // ...and give them some nice associations here!
+Artist.hasMany(Album);
+Album.belongsTo(Artist);
+
+Album.hasMany(Song);
+Song.belongsTo(Album)
+
 
 module.exports = {
-  db,
+  db, Album, Artist, Song
   // Include your models in your module.exports as well!
   // The seed file expects to find them there!
 }
